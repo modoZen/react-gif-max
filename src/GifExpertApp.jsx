c465ba283dbf8a04bc3fc0eac248f4f4e505291b
Max <mdiazn@uni.pe>
2022-09-26 16:05:07 -0500

    lista de categories

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 
 export const GifExpertApp = () => {
-  return (
-    <>
-        {/* titulo */}
-        <h1>GifExpertApp</h1>
 
-        {/* input */}
-        
-        {/* Listado de Gif */}
-            {/* Gif Item */}
-    </>
-  )
+    const [categories, setCategories] = useState(['Naruto', 'Dragon Ball', 'Bleach']);
+
+    return (
+        <>
+            {/* titulo */}
+            <h1>GifExpertApp</h1>
+
+            {/* input */}
+            
+            {/* Listado de Gif */}
+            <ol>
+                {categories.map( category =>(
+                    <li key={category} >{category}</li>
+                ))}
+            </ol>
+                {/* Gif Item */}
+        </>
+    )
 }
