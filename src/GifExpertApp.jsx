c4a0a9212199db1885805af4646cad95dc6c60db
Max <mdiazn@uni.pe>
2022-09-26 16:50:05 -0500

    agregar cateogria

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,6 +4,12 @@ export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['Naruto', 'Dragon Ball', 'Bleach']);
 
+    const onAddCategory = () => {
+        const newCategories = [...categories];
+        newCategories.push('One Punch Man');
+        setCategories( newCategories );
+    }
+
     return (
         <>
             {/* titulo */}
@@ -12,6 +18,9 @@ export const GifExpertApp = () => {
             {/* input */}
             
             {/* Listado de Gif */}
+            <button
+                onClick={onAddCategory} 
+            >Agregar</button>
             <ol>
                 {categories.map( category =>(
                     <li key={category} >{category}</li>
