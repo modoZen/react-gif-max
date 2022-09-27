806e24553961f7c8b802344244bb2b46488b0a6e
Max <mdiazn@uni.pe>
2022-09-26 23:23:59 -0500

    mostrar titulo de imagen

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,7 +4,7 @@ import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['Naruto']);
+    const [categories, setCategories] = useState([]);
 
     const onAddCategory = (newValue) => {
 
--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,24 @@
-import React, { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
-
+  const [images, setImages] = useState([]);
   useEffect( () => {
-    getGifs(category);
+    const getImages = async ()=> {
+      const newImageas =  await getGifs(category);
+      setImages( newImageas );
+    }
+    getImages();
   }, [] )
-
 
   return (
     <>
         <h3>{ category }</h3>
+        <ol>
+          {images.map(image=>(
+            <li>{image.title}</li>
+          ))}
+        </ol>
     </>
   )
 }
